8624c74ae5a8715cc7b35e682df057a5b1d6ec80
lijiangtao <3530812934@qq.com>
2025-10-12 10:46:39 +0800

    fix: 去掉鼠标移入logo显示最近登录时间提示

--- src/views/Layout/components/Logo/index.vue
+++ src/views/Layout/components/Logo/index.vue
@@ -1,36 +1,13 @@
 <script setup lang="ts">
 import logoInfo from '@/setting'
-import { useUserStore } from '@/stores/user'
-import { ref, onBeforeUnmount } from 'vue'
-import dayjs from 'dayjs'
 
-const userStore = useUserStore()
-const currentTime = ref('')
-const getTime = () => {
-  const now = new Date()
-  currentTime.value = dayjs(now).format('YYYY-MM-DD HH:mm:ss')
-}
-const timerId = setInterval(() => {
-  getTime()
-}, 1000)
-onBeforeUnmount(() => {
-  clearInterval(timerId)
-})
+
 </script>
 
 <template>
   <div class="logo">
     <el-image class="img" :src="logoInfo.logo" alt="logo" />
-    <div class="userinfo">
-      <el-image class="avator" :src="userStore.userInfo?.checkUser.avatar" />
-      <div class="userinfo-name">
-        欢迎您! {{ userStore.userInfo?.checkUser.username }}
-      </div>
-      <div>当前时间</div>
-      <div class="time">{{ currentTime }}</div>
-      <div>最近一次登录时间</div>
-      <div>{{ userStore.loginTime }}</div>
-    </div>
+    
     <h2>{{ logoInfo.title }}</h2>
   </div>
 </template>
@@ -50,7 +27,7 @@ onBeforeUnmount(() => {
   transition: all 0.5s;
   perspective: 500px;
   &:hover {
-    scale: 1.2;
+    scale: 0.9;
     z-index: 999;
     transform: translateZ(100px);
     box-shadow: 5px 5px 10px rgba(128, 128, 128, 0.5);
